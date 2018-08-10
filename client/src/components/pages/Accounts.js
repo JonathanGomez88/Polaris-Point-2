b2bb8f57d013d5b262c334a72dd03651d162f620
Trenton Ankenbruck <trenton15@outlook.com>
2018-08-10 12:57:37 -0400

    bugfix

--- client/src/components/pages/Accounts.js
+++ client/src/components/pages/Accounts.js
@@ -1,16 +1,15 @@
 import React from "react";
 
-const Accounts = () => ( <
-    div >
-    <
-    h1 > Account Page < /h1> <
-    p >
+const Accounts = () => ( 
+<div>
+
+    <h1> Account Page </h1> 
+<p>
     This is the page that is used
     for viewing users accounts.
 
-    <
-    /p> < /
-    div >
+</p> 
+</div>
 );
 
 export default Accounts;
