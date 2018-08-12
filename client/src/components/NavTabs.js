0d5101a2acd34986f552dd363a4b3f022fc51987
Trenton Ankenbruck <trenton15@outlook.com>
2018-08-11 20:04:08 -0400

    loginstuffnotworkingbutwillbesooni hope

--- client/src/components/NavTabs.js
+++ client/src/components/NavTabs.js
@@ -3,55 +3,42 @@ import {
     Link
 } from "react-router-dom";
 
-const NavTabs = () => ( <
-    ul className = "nav nav-tabs" >
-    <
-    li className = "nav-item" >
-    <
-    Link to = "/"
-    className = {
-        window.location.pathname === "/" ? "nav-link active" : "nav-link"
-    } >
-    Home <
-    /Link> < /
-    li > <
-    li className = "nav-item" >
-    <
-    Link to = "/about"
-    className = {
+const NavTabs = () => ( 
+    <ul className = "nav nav-tabs" >
+        <li className="nav-item"><Link to="login" 
+        className = {
+            window.location.pathname === "/login" ? "nav-link active" : "nav-link"}
+            >Login</Link></li>
+        <li className = "nav-item" >
+    <Link to = "/" className = {
+         window.location.pathname === "/" ? "nav-link active" : "nav-link"
+    }>Home </Link> 
+    </li > 
+    <li className = "nav-item" >
+    <Link to = "/about" className = {
         window.location.pathname === "/about" ? "nav-link active" : "nav-link"
-    } >
-    About <
-    /Link> < /
-    li > <
-    li className = "nav-item" >
-    <
-    Link to = "/Accounts"
+    }>About </Link> 
+    </li> <li className = "nav-item" >
+    <Link to = "/Accounts"
     className = {
         window.location.pathname === "/accounts" ? "nav-link active" : "nav-link"
     } >
-    Accounts <
-    /Link> < /
-    li > <
-    li className = "nav-item" >
-    <
-    Link to = "/contact"
+    Accounts </Link> 
+    </li> 
+    <li className = "nav-item" >
+    <Link to = "/contact"
     className = {
         window.location.pathname === "/contact" ? "nav-link active" : "nav-link"
     } >
-    Contact <
-    /Link> < /
-    li > <
-    li className = "nav-item" >
-    <
-    Link to = "/contact/learn"
+    Contact </Link> </li> 
+    <li className = "nav-item" >
+    <Link to = "/contact/learn"
     className = {
         window.location.pathname === "/contact/learn" ? "nav-link active" : "nav-link"
     } >
-    Learn <
-    /Link> < /
-    li > <
-    /ul>
+    Learn </Link> 
+    </li> 
+    </ul>
 );
 
 export default NavTabs;
