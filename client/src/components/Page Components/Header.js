2d6bcf9a6fa6ce6db9df30f27e15424f0adf8b8f
PawelLyko <pawel_lyko@yahoo.com>
2018-08-13 12:17:17 -0400

    adding some components that need to be styled

---
+++ client/src/components/Page Components/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Header = () => (
+  <div>
+    
+    <nav>
+    <link href="#" className="fa fa-facebook"></link>
+    <link href="#" className="fa fa-twitter"></link>
+    <link href="#" className="fa fa-linkedin"></link>
+
+    <button className="dropDown"/>
+    <div className="dropDownContent"> 
+    <link id="Astr"> Astronomy</link>
+    <link id="Geol"> Geology</link>
+    <link> {backendCourse[i]} </link>
+    </div>
+     </nav>
+
+
+  </div>
+);
+
+export default Header;
