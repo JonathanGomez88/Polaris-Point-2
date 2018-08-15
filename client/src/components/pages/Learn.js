7e4e9932df0dc3d9676d7a214dac2cf45a5835a4
Jongomez <jongomez@comcast.net>
2018-08-15 11:07:26 -0400

    stashing

--- client/src/components/pages/Learn.js
+++ client/src/components/pages/Learn.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import API from "../utils/API";
-import QuizQuestion from "../pageComponents/quizQuestions";
+import QuizQuestion from "../pageComponents/QuizQuestions";
 
 
 
